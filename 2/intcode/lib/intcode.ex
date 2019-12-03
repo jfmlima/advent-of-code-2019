1f1d8b6b77d811a6ded835563caa8ff6b91c0200
Jorge Lima <jorgefilipemlima@gmail.com>
2019-12-03 00:41:15 +0000

    Elixir solution for day 2 of Advent of Code 2019

---
+++ 2/intcode/lib/intcode.ex
@@ -0,0 +1,53 @@
+defmodule Intcode do
+  def part_1() do
+    parse_input()
+    |> part_1
+  end
+
+  @spec part_1(any) :: any
+  def part_1(values) do
+    rec(values, 0)
+  end
+
+  defp rec(values, idx) do
+    op = Enum.at(values, idx)
+
+    case op do
+      99 -> values
+      1 -> calc(fn x, y -> x + y end, values, idx)
+      2 -> calc(fn x, y -> x * y end, values, idx)
+    end
+  end
+
+  defp calc(ft, values, idx) do
+    f = Enum.at(values, Enum.at(values, idx + 1))
+    s = Enum.at(values, Enum.at(values, idx + 2))
+    pos = Enum.at(values, idx + 3)
+    rec(List.replace_at(values, pos, ft.(f, s)), idx + 4)
+  end
+
+  defp parse_input() do
+    File.read!("../input.txt")
+    |> String.split(",")
+    |> Enum.map(&String.to_integer(&1))
+  end
+
+  def part_2() do
+    input = parse_input()
+
+    for i <- 0..99 do
+      for j <- 0..99 do
+        values = input
+        t = List.replace_at(values, 1, i)
+        nl = List.replace_at(t, 2, j)
+        val = Enum.at(part_1(nl), 0)
+
+        cond do
+          val == 19690720 -> [i, j]
+          :else -> 0
+        end
+      end
+    end
+  end
+  
+end
