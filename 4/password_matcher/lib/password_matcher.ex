c844d9ba4888c1fd80e0e2dfbee2bab50a253406
Jorge Lima <jorgefilipemlima@gmail.com>
2019-12-09 23:25:30 +0000

    Add solution for part 2 of day 4

--- 4/password_matcher/lib/password_matcher.ex
+++ 4/password_matcher/lib/password_matcher.ex
@@ -1,30 +1,38 @@
 defmodule PasswordMatcher do
-
   def part_1() do
-    IO.inspect(count_matches("367479", 0))
+    count_matches("367479", 0)
   end
 
   defp count_matches("893698", c) do c end
 
   defp count_matches(b, c) do
     i = Integer.to_string(String.to_integer(b) + 1)
-    if(matches(b, 0)) do
-      count_matches(i, c + 1)
-    else count_matches(i, c) end
+
+    if(matches(b, 0)) do count_matches(i, c + 1) else count_matches(i, c) end
   end
 
   defp matches(_, -1) do false end
 
-  defp matches(<<f :: binary-size(1)>> <> <<s :: binary-size(1)>> <> "", c) do
-    (f < s && c > 4) || (f == s && c > 3)
+  defp matches(<<f::binary-size(1)>> <> <<s::binary-size(1)>> <> "", c) do (f < s && c > 4) || (f == s && c > 3) end
+
+  defp matches(<<f::binary-size(1)>> <> <<f::binary-size(1)>> <> t, c) do matches(f <> t, c + 2) end
+
+  defp matches(<<f::binary-size(1)>> <> <<s::binary-size(1)>> <> t, c) do if f < s do matches(s<>t, c + 1) else matches("", -1) end end
+
+  def part_2() do
+    367479..893698
+    |> Enum.filter(&is_sequential(Integer.to_string(&1)))
+    |> Enum.map(&Integer.digits(&1))
+    |> Enum.map(fn digits -> Enum.reduce(digits, %{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end ) end)
+    |> Enum.filter(&(Enum.member?(Map.values(&1), 2)))
+    |> Enum.count
   end
 
-  defp matches(<<f :: binary-size(1)>> <> <<s :: binary-size(1)>> <> t, c) do
-    n = s<>t
-    cond do
-      f < s -> matches(n, c+1)
-      f == s -> matches(n, c+2)
-      true -> matches(n, -1)
-    end
+  defp is_sequential(<<f::binary-size(1)>> <> <<s::binary-size(1)>>) do
+    f < s || f == s
+  end
+
+  defp is_sequential(<<f::binary-size(1)>> <> <<s::binary-size(1)>> <> t) do
+    if(f < s || f == s) do is_sequential(s <> t) else false end
   end
 end
